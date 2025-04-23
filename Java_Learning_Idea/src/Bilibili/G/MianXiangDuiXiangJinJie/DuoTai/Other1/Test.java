805d18371cce94cf940e8586e644124ddc125442
st 零 <435029375@qq.com>
2025-04-23 17:35:45 +0800

    [2025_4_23]

--- Java_Learning_Idea/src/Bilibili/G/MianXiangDuiXiangJinJie/DuoTai/Other1/Test.java
+++ Java_Learning_Idea/src/Bilibili/G/MianXiangDuiXiangJinJie/DuoTai/Other1/Test.java
@@ -6,8 +6,17 @@ public class Test {
         Student s = new Student();
         Teacher t = new Teacher();
         Administrator a =new Administrator();
-
-
+        s.show();
+        t.show();
+        a.show();
+        s.study();
+        //多态的使用
+        Person s1 = new Student();
+        s1.show();
+        //s1.study();  -->本行会报错，因为使用多态定义的成员变量无法使用自己独有的方法
+        Student s2 = (Student) s1;
+        s2.show();
+        s2.study();
 
 
     }
@@ -23,6 +32,10 @@ class Student extends Person{
     @Override
     public void show(){
         System.out.println("学生");
+    }
+    //Student类特有方法
+    public void study(){
+        System.out.println("学习");
     }
 }
 class Teacher extends Person{
